1e30d215684604faed77eddfe7649d645c215ecb
Misaka_GTX1060_Ubuntu <1150235785@qq.com>
2022-10-25 14:50:58 +0800

    Change format

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <assert.h>
 
-int main(int argc, char *argv[])
+int main(int argc, char** argv)
 {
-    for (int i = 0; i < argc; i++)
+    for (int i = 0; i < argc; ++i)
     {
         assert(argv[i]);
         printf("argv[%d] = %s\n", i, argv[i]);
